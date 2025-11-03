cfed485fea157b7672850bc6aafee0d51843ff04
Terry Fei <774542602@qq.com>
2025-11-03 12:59:15 +0800

    Update sum_map_test.go
    Signed-off-by: Terry Fei <774542602@qq.com>

--- optimizer/sum_map_test.go
+++ optimizer/sum_map_test.go
@@ -3,12 +3,12 @@ package optimizer_test
 import (
 	"testing"
 
-	"github.com/expr-lang/expr/internal/testify/assert"
-	"github.com/expr-lang/expr/internal/testify/require"
+	"github.com/fbatis/expr/internal/testify/assert"
+	"github.com/fbatis/expr/internal/testify/require"
 
-	"github.com/expr-lang/expr/ast"
-	"github.com/expr-lang/expr/optimizer"
-	"github.com/expr-lang/expr/parser"
+	"github.com/fbatis/expr/ast"
+	"github.com/fbatis/expr/optimizer"
+	"github.com/fbatis/expr/parser"
 )
 
 func TestOptimize_sum_map(t *testing.T) {
