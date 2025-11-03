0f71a9015d99135202dfb3965ee258c0cfb7e1eb
Terry Fei <774542602@qq.com>
2025-11-03 17:27:19 +0800

    Update interface_test.go
    Signed-off-by: Terry Fei <774542602@qq.com>

--- test/interface/interface_test.go
+++ test/interface/interface_test.go
@@ -3,8 +3,8 @@ package interface_test
 import (
 	"testing"
 
-	"github.com/expr-lang/expr"
-	"github.com/expr-lang/expr/internal/testify/assert"
+	"github.com/fbatis/expr"
+	"github.com/fbatis/expr/internal/testify/assert"
 )
 
 type StoreInterface interface {
