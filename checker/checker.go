fc679627c1250ecc1184bf5e9e0c6d0c7ad72f1f
Terry Fei <774542602@qq.com>
2025-11-03 12:47:21 +0800

    Update checker.go
    Signed-off-by: Terry Fei <774542602@qq.com>

--- checker/checker.go
+++ checker/checker.go
@@ -6,12 +6,12 @@ import (
 	"regexp"
 	"time"
 
-	"github.com/expr-lang/expr/ast"
-	"github.com/expr-lang/expr/builtin"
-	. "github.com/expr-lang/expr/checker/nature"
-	"github.com/expr-lang/expr/conf"
-	"github.com/expr-lang/expr/file"
-	"github.com/expr-lang/expr/parser"
+	"github.com/fbatis/expr/ast"
+	"github.com/fbatis/expr/builtin"
+	. "github.com/fbatis/expr/checker/nature"
+	"github.com/fbatis/expr/conf"
+	"github.com/fbatis/expr/file"
+	"github.com/fbatis/expr/parser"
 )
 
 var (
