3b65947ec4c8a00b123dc43b8f8987d5c81dc688
Terry Fei <774542602@qq.com>
2025-11-03 17:29:30 +0800

    Update issue_test.go
    Signed-off-by: Terry Fei <774542602@qq.com>

--- test/issues/840/issue_test.go
+++ test/issues/840/issue_test.go
@@ -3,8 +3,8 @@ package issue_test
 import (
 	"testing"
 
-	"github.com/expr-lang/expr"
-	"github.com/expr-lang/expr/internal/testify/require"
+	"github.com/fbatis/expr"
+	"github.com/fbatis/expr/internal/testify/require"
 )
 
 func TestEnvFieldMethods(t *testing.T) {
