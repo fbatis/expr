63431758e8d8d35e9562b9c5afea32b4af209561
Terry Fei <774542602@qq.com>
2025-11-03 17:31:40 +0800

    Update change_ident_test.go
    Signed-off-by: Terry Fei <774542602@qq.com>

--- test/patch/change_ident_test.go
+++ test/patch/change_ident_test.go
@@ -3,12 +3,12 @@ package patch_test
 import (
 	"testing"
 
-	"github.com/expr-lang/expr/internal/testify/require"
+	"github.com/fbatis/expr/internal/testify/require"
 
-	"github.com/expr-lang/expr"
-	"github.com/expr-lang/expr/ast"
-	"github.com/expr-lang/expr/vm"
-	"github.com/expr-lang/expr/vm/runtime"
+	"github.com/fbatis/expr"
+	"github.com/fbatis/expr/ast"
+	"github.com/fbatis/expr/vm"
+	"github.com/fbatis/expr/vm/runtime"
 )
 
 func TestPatch_change_ident(t *testing.T) {
