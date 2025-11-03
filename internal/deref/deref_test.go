5f66f660e4c10baea6697123961f4ad4179756ce
Terry Fei <774542602@qq.com>
2025-11-03 12:51:41 +0800

    Update deref_test.go
    Signed-off-by: Terry Fei <774542602@qq.com>

--- internal/deref/deref_test.go
+++ internal/deref/deref_test.go
@@ -4,9 +4,9 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/expr-lang/expr/internal/testify/assert"
+	"github.com/fbatis/expr/internal/testify/assert"
 
-	"github.com/expr-lang/expr/internal/deref"
+	"github.com/fbatis/expr/internal/deref"
 )
 
 func TestDeref(t *testing.T) {
