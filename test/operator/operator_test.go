954966f4d56ee739bf0582e44c8f35fc3a3df4f7
Terry Fei <774542602@qq.com>
2025-11-03 17:30:52 +0800

    Update operator_test.go
    Signed-off-by: Terry Fei <774542602@qq.com>

--- test/operator/operator_test.go
+++ test/operator/operator_test.go
@@ -5,11 +5,11 @@ import (
 	"testing"
 	"time"
 
-	"github.com/expr-lang/expr/internal/testify/assert"
-	"github.com/expr-lang/expr/internal/testify/require"
+	"github.com/fbatis/expr/internal/testify/assert"
+	"github.com/fbatis/expr/internal/testify/require"
 
-	"github.com/expr-lang/expr"
-	"github.com/expr-lang/expr/test/mock"
+	"github.com/fbatis/expr"
+	"github.com/fbatis/expr/test/mock"
 )
 
 func TestOperator_struct(t *testing.T) {
